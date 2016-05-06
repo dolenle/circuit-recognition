02cff09303adf9c7c4c8ce177b0ee08aecb98985
Dolen Le <Dolen Le>
2016-05-06 02:09:49 -0400

    updated getGrid, uploaded new training data

--- appGUI/getGrid.m
+++ appGUI/getGrid.m
@@ -4,6 +4,7 @@ function [Xind, Yind] = getGrid(expected_rows, expected_cols, cktImg)
 % disp(path);
 % figure; imshow(cktImg);
 % scan = ~im2bw(imread(path),0.95);
+enableDebug = true;
 cktImg = ~cktImg;
 colPeaks = sum(cktImg);
 [Xpeaks, Xind] = findpeaks(colPeaks, 'minpeakheight', max(colPeaks)*0.7);
@@ -25,7 +26,7 @@ for ii = 1:length(Yind)
     belowS = row+spaceMargin;
     if(above > 0 && below < size(cktImg,1))
         if((sum(cktImg(above,Xrange)) + sum(cktImg(below,Xrange)) < 4) ...
-            || (sum(cktImg(aboveS,setdiff(1:size(cktImg,2),Xrange)))>100 || sum(cktImg(belowS,setdiff(1:size(cktImg,2),Xrange)))>100))
+            || (sum(cktImg(aboveS,setdiff(1:size(cktImg,2),Xrange)))>80 || sum(cktImg(belowS,setdiff(1:size(cktImg,2),Xrange)))>80))
             disp 'invalid row at'; disp(Yind(ii))
             badY = [badY ii];
         end
@@ -34,12 +35,30 @@ end
 Yind(badY) = [];
 Ypeaks(badY) = [];
 
+Yrange = [Yind-4 Yind-3 Yind-2 Yind-1 Yind Yind+1 Yind+2 Yind+3 Yind+4];
+badX = [];
+for ii = 1:length(Xind)
+    col = Xind(ii);
+    left = col-verifyMargin;
+    right = col+verifyMargin;
+    leftS = col-spaceMargin;
+    rightS = col+spaceMargin;
+    if(left > 0 && right < size(cktImg,2))
+        if((sum(cktImg(Yrange,left)) + sum(cktImg(Yrange,right)) < 4) ...
+            || (sum(cktImg(setdiff(1:size(cktImg,1),Yrange),leftS))>80 || sum(cktImg(setdiff(1:size(cktImg,1),Yrange),rightS))>80))
+            disp 'invalid column at'; disp(Xind(ii))
+            badX = [badX ii];
+        end
+    end
+end
+Xind(badX) = [];
+Xpeaks(badX) = [];
 %%
 disp 'combining nearby points'
-range = 15;
+Xrange = 15;
 Xremove = [];
 for ii = 1:length(Xind)
-    nearby = find(Xind(ii)-range < Xind & Xind(ii)+range > Xind);
+    nearby = find(Xind(ii)-Xrange < Xind & Xind(ii)+Xrange > Xind);
     if(length(nearby) > 1)
         Xremove = [Xremove nearby(2:end)];
         Xpeaks(nearby) = max(Xpeaks(nearby));
@@ -48,9 +67,10 @@ end
 Xind(unique(Xremove)) = [];
 Xpeaks(unique(Xremove)) = [];
 
+Yrange = 15;
 Yremove = [];
 for ii = 1:length(Yind)
-    nearby = find(Yind(ii)-range < Yind & Yind(ii)+range > Yind)';
+    nearby = find(Yind(ii)-Yrange < Yind & Yind(ii)+Yrange > Yind)';
     if(length(nearby) > 1)
         Yremove = [Yremove nearby(2:end)];
         Ypeaks(nearby) = max(Ypeaks(nearby));
@@ -70,6 +90,9 @@ if(length(Yind) ~= expected_rows)
     view(-90, 90);
     set(gca,'Xdir','reverse','Ydir','reverse')
     fprintf(2,'ERROR: Incorrect number of rows\n')
+    if(enableDebug)
+        disp 'STOP';
+    end
 end
 if(length(Xind) ~= expected_cols)
     figure; imshow(cktImg);
@@ -82,6 +105,9 @@ if(length(Xind) ~= expected_cols)
     view(-90, 90);
     set(gca,'Xdir','reverse','Ydir','reverse')
     fprintf(2,'ERROR: Incorrect number of columns\n')
+    if(enableDebug)
+        disp 'STOP';
+    end
 end
 %% Plot
 % figure; hold on;
